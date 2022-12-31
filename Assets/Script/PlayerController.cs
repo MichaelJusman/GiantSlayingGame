06196a785a4dbb600c2cb953af11b26f63d3dc9d
MichaelJusman <107088338+MichaelJusman@users.noreply.github.com>
2022-12-31 14:57:07 +1100

    some player controller stuff

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    Idle,
+    Moving,
+    Jumping,
+    Swinging,
+    Attacking,
+    Damaged,
+    Riding,
+    Boosting,
+    Die
+}
+
 public class PlayerController : Singleton<PlayerController>
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public PlayerState playerState;
+
+    public void ChangePlayerState(PlayerState _playerState)
     {
-        
+        playerState = _playerState;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        switch(playerState)
+        {
+            case PlayerState.Idle:
+                break;
+        }
     }
 }
